8344e8117513e8960236893ec1aff3755e2ca70f
Prithviraj Sukale <pvsukale@gmail.com>
2020-02-06 23:53:16 +0530

    Database cleaner

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ group :development do
 end
 
 group :test do
+  # Clean database for tests
+  gem 'database_cleaner-active_record'
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,19 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Database cleanup betweeen tests
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.start
+  end
+
+  config.append_after do
+    DatabaseCleaner.clean
+  end
 end
