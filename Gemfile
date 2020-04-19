06fd4721b04b29f0236205b29a1eb57e3b2ae081
Prithviraj Sukale <pvsukale@gmail.com>
2020-04-18 20:47:03 -0700

    mailcatcher

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ group :development, :test do
   gem 'factory_bot_rails'
   # Environment variables
   gem 'dotenv-rails'
+  # Mail
+  gem 'mailcatcher'
 end
 
 group :development do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # email
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.smtp_settings = { :address => 'localhost', :port => 1025 }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
