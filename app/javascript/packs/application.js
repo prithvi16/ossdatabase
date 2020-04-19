64ba143e21fb96c7b3e1847da37f612ec9111b30
Prithviraj Sukale <pvsukale@gmail.com>
2020-04-18 20:10:17 -0700

    Added JQuery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
+require("jquery")
 import '../css/application.css'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
