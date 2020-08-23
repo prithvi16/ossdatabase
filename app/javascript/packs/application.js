3e4407e21e1514c568938ffa68da6087ebb26e12
Prithviraj Sukale <pvsukale@gmail.com>
2020-08-23 23:26:49 +0530

    Added tailwind css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 
 require("jquery")
-import '../css/application.css'
+import '../stylesheets/application.scss'
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
