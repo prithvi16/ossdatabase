2b057b38cf6b91917bd6db1668bf39939de4a608
Prithviraj Sukale <pvsukale@gmail.com>
2020-08-24 10:46:56 +0530

    Prettier is here

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
-require("jquery")
-import '../stylesheets/application.scss'
-
+require("jquery");
+import "../stylesheets/application.scss";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
