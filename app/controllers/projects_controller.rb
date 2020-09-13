fd94f0d5cd9352c8aa4eeec7cb7b0950b17d4475
Prithviraj Sukale <pvsukale@gmail.com>
2020-09-13 18:24:07 +0530

    edit project page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,6 +7,20 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @project = Project.new(project_params)
     if @project.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'projects/search', to: "projects#search", as: "projects_search"
-  resources :projects, only: [:new, :show, :create]
+  resources :projects, only: [:new, :show, :create, :edit, :update]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
