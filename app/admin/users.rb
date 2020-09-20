4b8b13a10f907fab36c87e98f80ca57026e917cf
Prithviraj Sukale <pvsukale@gmail.com>
2020-09-20 20:38:18 +0530

    Added username in forms

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register User do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
+  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :username
   #
   # or
   #
