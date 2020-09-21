db5539bc4c9164b38111abd6e58c302351abbd76
Prithviraj Sukale <pvsukale@gmail.com>
2020-09-21 10:42:15 +0530

    More specs

---
+++ spec/requests/pages_request_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "Pages", type: :request do
+
+  before(:each) do 
+    @sample_project = FactoryBot.create(:project)
+  end 
+
+  describe "GET home page" do
+    it "returns http success" do
+      get root_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "sign up page" do
+    it "returns http success" do
+      get new_user_registration_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "get login page" do
+    it "returns http success" do
+      get new_user_session_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
