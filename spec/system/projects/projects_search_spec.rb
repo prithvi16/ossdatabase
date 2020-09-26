d271c0f460aef5638459bf947a722cd189638fdf
Prithviraj Sukale <pvsukale@gmail.com>
2020-09-26 16:17:36 +0530

    fix specs

---
+++ spec/system/projects/projects_search_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "system_helper"
+
+describe "Projects search" do
+
+  before(:each) do 
+      @sample_project = FactoryBot.create(:project)
+  end 
+
+  it "Searches project and visits the project" do
+    visit projects_search_path
+    fill_in 'q_name_or_description_cont', with: @sample_project.name
+    click_button 'Search'
+    expect(page).to have_content(@sample_project.name)
+    expect(page).to have_link(nil, href: project_path(@sample_project))
+    click_link(nil, href: project_path(@sample_project))
+    expect(page).to have_content(@sample_project.name)
+  end
+
+end
