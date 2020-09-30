f1153da16c1c8a2742948f7c2594781d3a8502be
Prithviraj Sukale <pvsukale@gmail.com>
2020-09-30 17:59:03 +0530

    Usability improvements

--- app/components/project_component.rb
+++ app/components/project_component.rb
@@ -13,6 +13,14 @@ class ProjectComponent < ViewComponent::Base
   end
 
   def release_description
-    @project.first_release.strftime("%B, %Y") + " - " + @project.last_release.strftime("%B, %Y")
+    if @project.first_release? && @project.last_release?
+      @project.first_release.strftime("%B, %Y") + " - " + @project.last_release.strftime("%B, %Y")
+    elsif @project.first_release?
+      @project.first_release.strftime("%B, %Y") + " - -- "
+    elsif @project.last_release?
+      " - -- " + @project.last_release.strftime("%B, %Y")
+    else
+      "--"
+    end
   end
 end
