3b69a85ca1a753abc28a1194950b0f600260d71d
Prithviraj Sukale <pvsukale@gmail.com>
2020-09-30 14:30:57 +0530

    Fixed project list

--- app/components/project_list_component.rb
+++ app/components/project_list_component.rb
@@ -2,4 +2,12 @@ class ProjectListComponent < ViewComponent::Base
   def initialize(project_list:)
     @project_list = project_list
   end
+
+  def first_release(project)
+    if project.first_release.present?
+      time_ago_in_words(project.first_release)
+    else
+      "--"
+    end
+  end
 end
