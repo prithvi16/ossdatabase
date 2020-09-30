d6d9be893d9e68a0292fa52aeecf6448b45ab949
Prithviraj Sukale <pvsukale@gmail.com>
2020-09-30 14:33:29 +0530

    fixed 404 not found for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,6 +5,10 @@ class TagsController < ApplicationController
 
   def show
     @tag = Tag.find_by(name: params[:tag_name])
+    if @tag.nil?
+      render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
+      return
+    end
     @tag_projects = @tag.projects.includes([:taggings, :tags]).page params[:page]
   end
 end
