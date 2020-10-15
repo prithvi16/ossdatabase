74138c53beaacc27bcf07cc98183dc08fd8d8ed8
Prithviraj Sukale <pvsukale@gmail.com>
2020-10-15 22:28:17 +0530

    Fixed friendly id for exisiting tags

--- db/migrate/20201015164555_add_slug_to_tags.rb
+++ db/migrate/20201015164555_add_slug_to_tags.rb
@@ -2,5 +2,6 @@ class AddSlugToTags < ActiveRecord::Migration[6.0]
   def change
     add_column :tags, :slug, :string
     add_index :tags, :slug, unique: true
+    Tag.find_each(&:save)
   end
 end
