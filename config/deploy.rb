f4b187b4c09796b1fc0458688cc438cde2b79202
Prithviraj Sukale <pvsukale@gmail.com>
2021-04-20 17:49:41 +0530

    Fix capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+lock "~> 3.16.0"
 
 set :application, "ossdatabase"
 set :repo_url, "git@github.com:prithvi16/ossdatabase.git"
