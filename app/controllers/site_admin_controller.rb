301e887a4090fbaffb6bd68d360fb07440312832
Prithviraj Sukale <pvsukale@gmail.com>
2021-08-27 18:56:46 +0530

    Many changes

--- app/controllers/site_admin_controller.rb
+++ app/controllers/site_admin_controller.rb
@@ -1,4 +1,22 @@
 class SiteAdminController < ApplicationController
+  before_action :authenticate_user!
+  before_action :is_user_admin
+
   def home
   end
+
+  def github_projects
+    github_projects = params[:github_projects]
+    GithubToProjectWorker.perform_async(github_projects)
+    flash[:success] = "Added to queue sucessfully"
+    redirect_to site_admin_home_path
+  end
+
+  private
+
+    def is_user_admin
+      if !current_user.admin?
+        redirect_to root_path
+      end
+    end
 end
--- app/workers/github_to_project_worker.rb
+++ app/workers/github_to_project_worker.rb
@@ -5,17 +5,18 @@ class GithubToProjectWorker
     github_client = Octokit::Client.new(access_token: ENV["GITHUB_API_ACCESS_TOKEN"])
     github_repo_data = github_client.repo project_github_string
     project_readme = github_client.readme project_github_string, accept: 'application/vnd.github.html'
-    project = Project.create!(
-      name: github_repo_data.name,
-      website: github_repo_data.html_url,
-      tag_line: github_repo_data.description,
-      description: project_readme,
-      visible: true,
-      reviewed: true,
-      source: "github",
-      source_id: github_repo_data.id,
-      last_updated_from_source: DateTime.now,
-      repo_url: github_repo_data.html_url
-    )
+    if !Project.where(source: "github", source_id: github_repo_data.id).exists?
+      project = Project.create!(
+        name: github_repo_data.name,
+        website: github_repo_data.html_url,
+        tag_line: github_repo_data.description,
+        description: project_readme,
+        visible: true,
+        reviewed: true,
+        source: "github",
+        source_id: github_repo_data.id,
+        last_updated_from_source: DateTime.now,
+        repo_url: github_repo_data.html_url)
+    end
   end
 end
