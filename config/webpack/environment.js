83d25a4f4772ae7ef129e3fd0917b25643ee48c7
Prithviraj Sukale <pvsukale@gmail.com>
2022-02-18 13:11:20 +0530

    Upgarde to TailwindCSS 2.0

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,4 +6,27 @@ environment.plugins.prepend('Provide',
     jQuery: 'jquery/src/jquery'
   })
 )
+
+// See https://github.com/rails/webpacker/issues/2784
+function hotfixPostcssLoaderConfig (subloader) {
+  const subloaderName = subloader.loader
+  if (subloaderName === 'postcss-loader') {
+    if (subloader.options.postcssOptions) {
+      console.log(
+        '\x1b[31m%s\x1b[0m',
+        'Remove postcssOptions workaround in config/webpack/environment.js'
+      )
+    } else {
+      subloader.options.postcssOptions = subloader.options.config;
+      delete subloader.options.config;
+    }
+  }
+}
+
+// see https://github.com/rails/webpacker/issues/2784
+environment.loaders.keys().forEach(loaderName => {
+  const loader = environment.loaders.get(loaderName);
+  loader.use.forEach(hotfixPostcssLoaderConfig);
+});
+
 module.exports = environment
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,32 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [
-    './app/**/*.html.erb',
+  mode: 'jit',
+  purge: ['./app/**/*.html.erb',
     './app/**/*.rb',
-    './config/initializers/*.rb'
+    './config/initializers/*.rb',
+    './app/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sky: colors.sky,
+        teal: colors.teal,
+      },
+    },
   },
-  variants: {},
-  plugins: [require('@tailwindcss/ui'),
+  variants: {
+    extend: {
+      fontWeight: ['hover', 'focus'],
+      backgroundColor: ['group-focus', 'active'],
+      borderColor: ['group-focus'],
+      boxShadow: ['group-focus'],
+      opacity: ['group-focus'],
+      textColor: ['group-focus', 'active'],
+      textDecoration: ['group-focus'],
+    },
+  },
+  plugins: [require('@tailwindcss/forms'),
             require('@tailwindcss/typography'),
-    ],
+            require('@tailwindcss/aspect-ratio'),],
 }
