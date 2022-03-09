baac0328b7a88ca1e5ceb11c338bb7899ea1f062
Prithviraj Sukale <pvsukale@gmail.com>
2022-03-09 23:57:09 +0530

    New colors

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,1 +1,8 @@
 TOP_TAG_TYPES = [ "license", "tech", "category", "usecase", "platform"]
+TAG_COLORS = {
+  "license" => "bg-cyan-500",
+  "tech" => "bg-green-500",
+  "category" => "bg-orange-500",
+  "usecase" => "bg-purple-500",
+  "platform" => "bg-red-500"
+}
