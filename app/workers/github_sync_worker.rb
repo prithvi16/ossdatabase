62843f83b95c93c5216bd27e077e546a6ff3201f
Prithviraj Sukale <pvsukale@gmail.com>
2022-03-25 00:36:18 +0530

    Updated sync worker

--- app/workers/github_sync_worker.rb
+++ app/workers/github_sync_worker.rb
@@ -4,7 +4,11 @@ class GithubSyncWorker
   def perform(project_id)
     project = Project.find(project_id)
     github_client = Octokit::Client.new(access_token: ENV["GITHUB_API_ACCESS_TOKEN"])
-    github_repo_data = github_client.repo project.source_id
-    project.update!(github_stars: github_repo_data.stargazers_count, repo_url: github_repo_data.html_url, last_updated_from_source: DateTime.now)
+    if project.github_id.present?
+      github_repo_data = github_client.repo project.github_id
+    else
+      github_repo_data = github_client.repo project.source_id
+    end
+    project.update!(github_stars: github_repo_data.stargazers_count, github_id: github_repo_data.id, source_id: github_repo_data.full_name, repo_url: github_repo_data.html_url, last_updated_from_source: DateTime.now)
   end
 end
