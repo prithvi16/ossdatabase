1596ea6f380d16794a4293e2ef40183995abbd9e
Prithviraj Sukale <pvsukale@gmail.com>
2022-04-29 21:05:00 +0530

    Added Daily sync worker

---
+++ app/workers/daily_repo_source_sync_worker.rb
@@ -0,0 +1,12 @@
+class DailyRepoSourceSyncWorker
+  include Sidekiq::Worker
+
+  def perform()
+    all_projects = Project.where(source: "github").where("source_id is not null")
+    counter = 0
+    all_projects.each do |project|
+      counter += 5
+      GithubSyncWorker.perform_in(counter.seconds, project.id)
+    end
+  end
+end
