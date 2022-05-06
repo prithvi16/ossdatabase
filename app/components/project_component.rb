ac2a34f597f316252dcb800cbfa820b61c59b7c6
Prithviraj Sukale <pvsukale@gmail.com>
2022-05-06 14:26:08 +0530

    New link attributes

--- app/components/project_component.rb
+++ app/components/project_component.rb
@@ -2,7 +2,7 @@ class ProjectComponent < ViewComponent::Base
   def initialize(project:, current_user:)
     @project = project
     @current_user = current_user
-    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, link_attributes: { target: "_blank", rel: "noopener noreferrer nofollow" })
   end
 
   def premium_description
