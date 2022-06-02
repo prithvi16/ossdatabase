7f84e622e3a1a68d871c000aca5e8e8fb0a59bb8
Prithviraj Sukale <pvsukale@gmail.com>
2022-06-02 16:46:32 +0530

    New submission spec

--- spec/system/projects/projects_create_spec.rb
+++ spec/system/projects/projects_create_spec.rb
@@ -2,7 +2,7 @@
 
 require "system_helper"
 
-describe "Projects create" do
+describe "Projects submission" do
   it "Shows errors when project is invalid" do
     visit new_submission_path
 
@@ -11,5 +11,8 @@ describe "Projects create" do
     fill_in "Tagline", with: example_project.tag_line
     fill_in "About project*", with: example_project.description
     fill_in "Project website", with: example_project.website
+
+    expect { click_button "Submit Project →" }.to change { Submission.count }.by(1)
+    expect(page).to have_content("Thank you for your submission!")
   end
 end
