1f99dcb5f2e6197db1e7a3151c3eb506971d83d3
Prithviraj Sukale <pvsukale@gmail.com>
2022-06-08 12:55:31 +0530

    Improved search

--- app/javascript/controllers/nav_search_controller.js
+++ app/javascript/controllers/nav_search_controller.js
@@ -1,36 +1,33 @@
 import { Controller } from "@hotwired/stimulus";
-import { useClickOutside } from 'stimulus-use'
+import { useClickOutside } from "stimulus-use";
 let debounce = require("lodash/debounce");
-
 
 export default class extends Controller {
   static targets = ["form", "resultParent"];
-  connect() 
-  {
+  connect() {
     this.triggerSearch = debounce(this.triggerSearch, 500).bind(this);
   }
 
   clickOutside(event) {
     // example to close a modal
-    event.preventDefault()
+    event.preventDefault();
     this.resultParentTarget.classList.add("opacity-0", "-z-10");
     this.resultParentTarget.classList.remove("opacity-100", "z-10");
   }
-  
-
 
   triggerSearch(event) {
     var query = event.target.value;
-    $.ajax({ 
-      url: "/projects/search_suggestions",
-      data: { query: query },
-      method: "POST",
-      success: function(data) {
-        this.resultParentTarget.innerHTML = data;
-        this.resultParentTarget.classList.remove("opacity-0", "-z-10");
-        this.resultParentTarget.classList.add("opacity-100", "z-10");
-      }.bind(this)
-    });
+    if (query.length > 2) {
+      $.ajax({
+        url: "/projects/search_suggestions",
+        data: { query: query },
+        method: "POST",
+        success: function (data) {
+          this.resultParentTarget.innerHTML = data;
+          this.resultParentTarget.classList.remove("opacity-0", "-z-10");
+          this.resultParentTarget.classList.add("opacity-100", "z-10");
+        }.bind(this),
+      });
+    }
   }
-
 }
