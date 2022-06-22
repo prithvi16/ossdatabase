5c8821c2e9e27a3755ecb469ebf4821084db3af4
Prithviraj Sukale <pvsukale@gmail.com>
2022-06-22 13:40:17 +0530

    Active paths

--- app/components/open_source_guide_nav_component.rb
+++ app/components/open_source_guide_nav_component.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class OpenSourceGuideNavComponent < ViewComponent::Base
+  def active_class(current_page)
+    if request.path == current_page
+      "bg-gray-300 text-gray-900"
+    else
+      "text-gray-600 hover:bg-gray-50 hover:text-gray-900"
+    end
+  end
 end
