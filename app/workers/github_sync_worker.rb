ccef95afe7f58db216e5fe8c562aa342a3b57481
Prithviraj Sukale <pvsukale@gmail.com>
2023-04-07 11:29:19 +0530

    Update github sync worker

--- app/workers/github_sync_worker.rb
+++ app/workers/github_sync_worker.rb
@@ -2,13 +2,29 @@ class GithubSyncWorker
   include Sidekiq::Worker
 
   def perform(project_id)
-    project = Project.find(project_id)
-    github_repo_data = if project.github_id.present?
-      GithubApi.client.repo project.github_id
+    @project = Project.find(project_id)
+    github_repo_data = fetch_repo_data
+    update_with_repo_data(github_repo_data)
+    parsed_github_data = fetch_graphql_data
+    update_with_graphql_data(parsed_github_data)
+    update_readme
+    attach_logo unless @project.avatar.attached?
+  end
+
+  def attach_logo
+    AttachGithubProjectLogoWorker.new.perform(@project.id)
+  end
+
+  def fetch_repo_data
+    if @project.github_id.present?
+      GithubApi.client.repo @project.github_id
     else
-      GithubApi.client.repo project.source_id
+      GithubApi.client.repo @project.source_id
     end
-    project.update!(github_open_issues_count: github_repo_data.open_issues_count,
+  end
+
+  def update_with_repo_data(github_repo_data)
+    @project.update!(github_open_issues_count: github_repo_data.open_issues_count,
       github_stars: github_repo_data.stargazers_count,
       github_id: github_repo_data.id,
       source_id: github_repo_data.full_name,
@@ -16,8 +32,14 @@ class GithubSyncWorker
       github_forks_count: github_repo_data.forks_count,
       github_watchers_count: github_repo_data.watchers_count,
       last_updated_from_source: DateTime.now)
-    parsed_github_data = GithubDataFetcherService.new(project.source_id).fetch_data
-    project.update!(
+  end
+
+  def fetch_graphql_data
+    GithubDataFetcherService.new(@project.source_id).fetch_data
+  end
+
+  def update_with_graphql_data(parsed_github_data)
+    @project.update!(
       github_commits_count: parsed_github_data.total_commits,
       github_closed_issues_count: parsed_github_data.total_closed_issues,
       github_open_pull_requests_count: parsed_github_data.open_pull_requests,
@@ -26,5 +48,9 @@ class GithubSyncWorker
       github_last_release_date: parsed_github_data.last_release_date,
       github_last_commit_date: parsed_github_data.last_commit_date
     )
+  end
+
+  def update_readme
+    GithubReadmeUpdaterService.new(@project.id).update_readme
   end
 end
